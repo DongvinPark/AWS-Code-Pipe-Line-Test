9dafe4778f8a0528d137886ec583680900716bbd
DongvinPark <dongvin99@naver.com>
2023-08-03 12:53:23 +0900

    initial commit

---
+++ src/main/java/com/example/nginxloadbalancing/Controller.java
@@ -0,0 +1,22 @@
+package com.example.nginxloadbalancing;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+  @GetMapping("/")
+  public ResponseEntity<String> healthCheck () {
+    return ResponseEntity.status(HttpStatus.OK).body("healty");
+  }
+
+  @PostMapping("/test-api")
+  public ResponseEntity<String> test () {
+    return ResponseEntity.status(HttpStatus.OK).body("test success");
+  }
+
+}
---
+++ src/test/java/com/example/nginxloadbalancing/NginxLoadbalancingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nginxloadbalancing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxLoadbalancingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
